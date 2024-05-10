1228cb59e1c8a7a27fb23abf71ff726fe3b50f88
Bhargava Kancherla <bhargavakumark@google.com>
2024-05-10 04:03:15 +0000

    updating init.lua with old vimrc

--- init.lua
+++ init.lua
@@ -142,7 +142,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -908,5 +908,297 @@ require('lazy').setup({
   },
 })
 
+-- Install Packer if not already installed
+local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/site/packer.nvim'
+if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
+  vim.api.nvim_command('!git clone https://github.com/wbthomason/packer.nvim ' .. packer_path)
+end
+vim.opt.rtp:prepend(packer_path)
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'kaicataldo/material.vim'
+  use 'ntpeters/vim-better-whitespace'
+end)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+vim.cmd [[
+set notimeout
+set encoding=utf-8
+
+set runtimepath^=~/.nvim runtimepath+=~/.nvim/after
+let &packpath = &runtimepath
+
+"set number relativenumber
+set number
+:nmap <F5> :set nonumber<CR>
+set exrc " .vimrc in local project dir
+set secure
+
+" When moving with ctrl+], this save buffer hidden
+set hidden
+set confirm
+
+set foldmethod=indent   "fold based on indent
+set foldnestmax=10      "deepest fold is 10 levels
+set nofoldenable        "dont fold by default
+set foldlevel=1         "this is just what i use
+
+set diffopt+=vertical   "start diff in vertical mode
+
+" both absolute and relative line numbers are enabled by default,
+" which produces “hybrid” line numbers. When entering insert mode,
+" relative line numbers are turned off, leaving absolute line numbers turned on.
+" This also happens when the buffer loses focus, so you can glance back at it
+" to see which absolute line you were working on if you need to.
+":augroup numbertoggle
+":  autocmd!
+":  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+":  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+":augroup END
+
+"Below for \t tab alignments
+":set shiftwidth=8
+":set tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab
+"Below for 4 space tab aligments
+":set tabstop=8 softtabstop=4 shiftwidth=4 noexpandtab
+:set shiftwidth=4 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+if has("autocmd")
+    " For java files use \t as separator
+    autocmd BufRead,BufNewFile *.java :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+    autocmd BufRead,BufNewFile *.js :set shiftwidth=8 tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab
+    autocmd BufRead,BufNewFile *.cpp :set shiftwidth=4 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+    "autocmd BufRead,BufNewFile *.cpp :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+    "autocmd BufRead,BufNewFile *.sh :set shiftwidth=4 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+    autocmd BufRead,BufNewFile *.sh :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+    autocmd BufRead,BufNewFile *bashrc_common :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+    autocmd BufRead,BufNewFile *.bash :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+    autocmd BufRead,BufNewFile gob :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+    autocmd BufRead,BufNewFile *.pl :set shiftwidth=8 tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab
+    autocmd BufRead,BufNewFile *.go :set shiftwidth=8 tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab
+    autocmd BufRead,BufNewFile *.yaml :set shiftwidth=2 tabstop=2 softtabstop=2 shiftwidth=2 expandtab
+endif
+
+" pathogen
+"execute pathogen#infect()
+
+" Use deoplete.
+let g:deoplete#enable_at_startup = 1
+"  Run UpdateRemotePlugins it not loading
+
+"-- TRUE COLOR --
+" For Neovim 0.1.3 and 0.1.4 - https://github.com/neovim/neovim/pull/2198
+if (has('nvim'))
+  let $NVIM_TUI_ENABLE_TRUE_COLOR = 1
+endif
+
+" For Neovim > 0.1.5 and Vim > patch 7.4.1799 - https://github.com/vim/vim/commit/61be73bb0f965a895bfb064ea3e55476ac175162
+" Based on Vim patch 7.4.1770 (`guicolors` option) - https://github.com/vim/vim/commit/8a633e3427b47286869aa4b96f2bfc1fe65b25cd
+" https://github.com/neovim/neovim/wiki/Following-HEAD#20160511
+if (has("termguicolors") && $TERM_PROGRAM ==# 'iTerm.app')
+  set termguicolors
+endif
+
+" pathogen
+"execute pathogen#infect()
+
+" Use deoplete.
+let g:deoplete#enable_at_startup = 1
+"  Run UpdateRemotePlugins it not loading
+
+"-- TRUE COLOR --
+" For Neovim 0.1.3 and 0.1.4 - https://github.com/neovim/neovim/pull/2198
+if (has('nvim'))
+  let $NVIM_TUI_ENABLE_TRUE_COLOR = 1
+endif
+
+" For Neovim > 0.1.5 and Vim > patch 7.4.1799 - https://github.com/vim/vim/commit/61be73bb0f965a895bfb064ea3e55476ac175162
+" Based on Vim patch 7.4.1770 (`guicolors` option) - https://github.com/vim/vim/commit/8a633e3427b47286869aa4b96f2bfc1fe65b25cd
+" https://github.com/neovim/neovim/wiki/Following-HEAD#20160511
+if (has("termguicolors") && $TERM_PROGRAM ==# 'iTerm.app')
+  set termguicolors
+endif
+
+"-- THEMING --
+set cursorline          " current line is underlined
+set background=dark
+
+let g:airline_theme='material'
+"let g:material_theme_style = 'darker'
+colorscheme material
+
+hi Normal       ctermbg=NONE guibg=NONE
+hi SignColumn   ctermbg=235 guibg=#262626
+hi LineNr       ctermfg=grey guifg=grey ctermbg=NONE guibg=NONE
+hi CursorLineNr ctermbg=NONE guibg=NONE ctermfg=178 guifg=#d7af00
+
+"-- Airline --
+let g:airline#extensions#tabline#enabled = 1
+
+let g:gitgutter_set_sign_backgrounds = 0
+
+" I have put this plugin in ~/.config/nvim/bundle , but it doesn't seem to
+" load
+"-- Whitespace highlight --
+"match ExtraWhitespace /\s\+$/
+autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
+autocmd InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+autocmd InsertLeave * match ExtraWhitespace /\s\+$/
+autocmd BufWinLeave * call clearmatches()
+
+": spell corrections
+:iabbrev teh the
+:iabbrev aer are
+:iabbrev etst test
+:iabbrev shoudl should
+:iabbrev lenght length
+:iabbrev unxi unix
+:iabbrev ofr for
+:iabbrev disbale disable
+:iabbrev meida media
+:iabbrev Meida Media
+:iabbrev hte the
+:iabbrev deivce device
+:iabbrev optinos options
+:iabbrev referenece reference
+:iabbrev witdh width
+:iabbrev ouput output
+:iabbrev prinft printf
+:iabbrev evn env
+:iabbrev gruop group
+:iabbrev updrestore udprestore
+:iabbrev hcm hmc
+:iabbrev hcmHost hmcHost
+:iabbrev hcmLpar hmcLpar
+:iabbrev iamge image
+:iabbrev virutal virtual
+:iabbrev strint string
+:iabbrev ERORR ERROR
+:iabbrev instnace instance
+
+" set NERDTreeWinPos="right"		" Doesn't work anymore
+
+" Sometimes when vim loads, there are errors, these errors don't show up
+" if nerdtree is enabled. Disable NERDTree and start again to see errors
+" Start NERDTree
+"autocmd VimEnter * NERDTree
+" Start NERDTree and put the cursor back in the other window.
+"autocmd VimEnter * wincmd p
+
+"autocmd VimEnter * wincmd p
+":NERDTreeToggle
+
+:set wildmode=list:longest
+:set winheight=9999
+:set so=5
+
+nnoremap <leader>n :NERDTreeFocus<CR>
+"nnoremap <C-n> :NERDTree<CR>
+"nnoremap <C-t> :NERDTreeToggle<CR>
+:nmap <F4> :NERDTreeToggle<CR>
+"nnoremap <C-f> :NERDTreeFind<CR>
+
+" Close the tab if NERDTree is the only window remaining in it.
+autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+
+" Mirror the NERDTree before showing it. This makes it the same on all tabs.
+nnoremap <C-n> :NERDTreeMirror<CR>:NERDTreeFocus<CR>
+
+
+" open file at the same line as we closed
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+endif
+
+""-- ALE --
+"hi clear ALEErrorSign
+"hi clear ALEWarningSign
+"let g:ale_echo_msg_format = '[%linter%] %s [%severity%]'
+"let g:ale_sign_error = '‚úò'
+"let g:ale_sign_warning = '‚óã'
+"hi Error    ctermfg=204 ctermbg=NONE guifg=#ff5f87 guibg=NONE
+"hi Warning  ctermfg=178 ctermbg=NONE guifg=#D7AF00 guibg=NONE
+"hi ALEError ctermfg=204 guifg=#ff5f87 ctermbg=52 guibg=#5f0000 cterm=undercurl gui=undercurl
+"hi link ALEErrorSign    Error
+"hi link ALEWarningSign  Warning
+"
+"let g:ale_linters = {
+"            \ 'python': ['pylint'],
+"            \ 'javascript': ['eslint'],
+"            \ 'go': ['gobuild', 'gofmt'],
+"            \ 'rust': ['rls']
+"            \}
+"let g:ale_fixers = {
+"            \ '*': ['remove_trailing_lines', 'trim_whitespace'],
+"            \ 'python': ['autopep8'],
+"            \ 'javascript': ['eslint'],
+"            \ 'go': ['gofmt', 'goimports'],
+"            \ 'rust': ['rustfmt']
+"            \}
+"let g:ale_fix_on_save = 1
+"
+""-- NERDTree --
+"let NERDTreeShowHidden=1
+"
+""-- Airline --
+"let g:airline#extensions#tabline#enabled = 1
+"
+""uo-- NVIM configuration --
+"if has('nvim')
+"    " Enable deoplete when InsertEnter.
+"    let g:deoplete#enable_at_startup = 0
+"    autocmd InsertEnter * call deoplete#enable()
+"
+"    set belloff=""
+"    call deoplete#custom#source('_',  'max_menu_width', 0)
+"    call deoplete#custom#source('_',  'max_abbr_width', 0)
+"    call deoplete#custom#source('_',  'max_kind_width', 0)
+"
+"    set hidden
+"    let g:LanguageClient_serverCommands = {
+"        \ 'rust': ['~/.cargo/bin/rustup', 'run', 'stable', 'rls'],
+"        \ 'go': ['~/.go/bin/gopls']
+"        \ }
+"endif
+
+" Disable gutenttags run on file close
+let g:gutentags_generate_on_write=0
+
+
+set tags=tags;/
+set tags+=~/.vim/tags/cpp_src
+"helptags ~/.vim/doc
+
+"if has("cscope")
+"  set csto=1
+"  set nocst
+"  set nocsverb
+"  " add any database in current directory
+"  if filereadable("cscope.out")
+"    cs add cscope.out
+"    " else add database pointed to by environment
+"  elseif $CSCOPE_DB != ""
+"    cs add $CSCOPE_DB
+"  endif
+"  set csverb
+"endif
+
+" https://vim.fandom.com/wiki/Autoloading_Cscope_Database
+function! LoadCscope()
+  let db = findfile("cscope.out", ".;")
+  if (!empty(db))
+    let path = strpart(db, 0, match(db, "/cscope.out$"))
+    set nocscopeverbose " suppress 'duplicate connection' error
+    exe "cs add " . db . " " . path
+    set cscopeverbose
+  " else add the database pointed to by environment variable 
+  elseif $CSCOPE_DB != "" 
+    cs add $CSCOPE_DB
+  endif
+endfunction
+au BufEnter /* call LoadCscope()
+
+set ic
+]]
