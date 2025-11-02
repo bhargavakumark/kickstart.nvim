7d877b207cbda56474becd302d807de2b0dcbc72
bkancherla <bkancherla@salesforce.com>
2025-11-02 19:55:45 +0530

    Merging some config from old one

--- init.lua
+++ init.lua
@@ -296,7 +296,57 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go', 'gomod', 'gowork', 'gosum' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    config = function(_, opts)
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+      -- Prefer git instead of curl in order to improve connectivity in some environments
+      require('nvim-treesitter.install').prefer_git = true
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- There are additional nvim-treesitter modules that you can use to interact
+      -- with nvim-treesitter. You should go explore a few and see what interests you:
+      --
+      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    end,
+  },
+
+  {
+    'doums/darcula',
+  },
 })
+
+-- Allows snippets to handle <Tab> for final jumps. from hrsh7th/.*vsnip
+vim.g.vsnip_append_final_tabstop = 1 -- Lua
+vim.g.vsnip_deactivate_on = 'InsertLeave'
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { 'lua', 'python', 'go' },
+  highlight = {
+    enable = true,
+  },
+}
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'sh',
@@ -414,7 +464,8 @@ let g:airline_theme='material'
 "let g:material_theme_style = 'darker'
 "colorscheme material
 "colorscheme kanagawa
-colorscheme darcula-dark
+"colorscheme darcula-dark
+colorscheme darcula
 
 hi Normal       ctermbg=NONE guibg=NONE
 hi SignColumn   ctermbg=235 guibg=#262626
