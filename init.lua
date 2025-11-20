7c5f173da68cd2dfb00993143548ca9d222423a2
bkancherla <bkancherla@salesforce.com>
2025-11-20 11:43:08 +0530

    adding Telescope config

--- init.lua
+++ init.lua
@@ -344,6 +344,157 @@ require('lazy').setup({
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
   {'L3MON4D3/LuaSnip'},
+
+  { -- Fuzzy Finder (files, lsp, etc)
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
+
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+
+      -- Useful for getting pretty icons, but requires a Nerd Font.
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      -- Telescope is a fuzzy finder that comes with a lot of different things that
+      -- it can fuzzy find! It's more than just a "file finder", it can search
+      -- many different aspects of Neovim, your workspace, LSP, and more!
+      --
+      -- The easiest way to use Telescope, is to start by doing something like:
+      --  :Telescope help_tags
+      --
+      -- After running this command, a window will open up and you're able to
+      -- type in the prompt window. You'll see a list of `help_tags` options and
+      -- a corresponding preview of the help.
+      --
+      -- Two important keymaps to use while in Telescope are:
+      --  - Insert mode: <c-/>
+      --  - Normal mode: ?
+      --
+      -- This opens a window that shows you all of the keymaps for the current
+      -- Telescope picker. This is really useful to discover what Telescope can
+      -- do as well as how to actually do it!
+
+      -- [[ Configure Telescope ]]
+      -- See `:help telescope` and `:help telescope.setup()`
+      require('telescope').setup {
+        -- You can put your default mappings / updates / etc. in here
+        --  All the info you're looking for is in `:help telescope.setup()`
+        --
+        -- defaults = {
+        --   mappings = {
+        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        --   },
+        -- },
+        -- pickers = {}
+        extensions = {
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
+          },
+        },
+        -- In your Telescope setup file (e.g., ~/.config/nvim/lua/plugins/telescope.lua)
+
+        defaults = {
+          -- path_display = { "filename_first" }, -- Doesn't seem to work as expected
+          path_display = { "truncate" },
+          layout_config = {
+            preview_width = 0.4,
+            -- Setting padding to 0 for both width and height
+            -- tells Telescope to use the full available space minus status lines/tab lines.
+            width = 0.98,
+            height = 0.98,
+
+            -- Optional: You can also explicitly set a large percentage value close to 1
+            -- width = { padding = 0 },
+            -- height = { padding = 0 },
+            -- width = 0.95, -- 95% of the window width
+            -- height = 0.9, -- 90% of the window height
+          },
+          -- Key mappings apply to all pickers unless overridden
+          mappings = {
+            -- i (Insert) Mode Mappings
+            i = {
+              -- Set Enter (<CR>) to open the file in a new tab/buffer
+              ["<CR>"] = require('telescope.actions').select_tab,
+
+              -- Optional: Keep the standard 'select_default' on Shift+CR for flexibility
+              ["<S-CR>"] = require('telescope.actions').select_default,
+            },
+
+            -- n (Normal) Mode Mappings
+            n = {
+              -- Set Enter (<CR>) to open the file in a new tab/buffer
+              ["<CR>"] = require('telescope.actions').select_tab,
+
+              -- Optional: Map 't' to the tab action as well
+              ["t"] = require('telescope.actions').select_tab,
+            },
+          },
+        },
+        -- You can also override specific picker options if needed
+        pickers = {
+            find_files = {
+                -- The mappings defined in 'defaults' above will apply here
+            }
+        }
+      }
+
+      -- Enable Telescope extensions if they are installed
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+
+      -- See `:help telescope.builtin`
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      -- Slightly advanced example of overriding default behavior and theme
+      vim.keymap.set('n', '<leader>/', function()
+        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
+
+      -- It's also possible to pass additional configuration options.
+      --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      vim.keymap.set('n', '<leader>s/', function()
+        builtin.live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end, { desc = '[S]earch [/] in Open Files' })
+
+      -- Shortcut for searching your Neovim configuration files
+      vim.keymap.set('n', '<leader>sn', function()
+        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [N]eovim files' })
+    end,
+  },
+
 })
 
 -- Based on https://github.com/mfussenegger/nvim-jdtls?tab=readme-ov-file
@@ -859,3 +1010,42 @@ vim.api.nvim_create_autocmd('BufNewFile', {
     vim.cmd 'normal G' -- Move cursor to end
   end,
 })
+
+-- ========================================================
+-- Custom Function: openf (Open File Fuzzy Search)
+-- Requires: nvim-telescope/telescope.nvim
+-- ========================================================
+
+local telescope = require('telescope.builtin')
+
+function openf(pattern)
+  -- Default to searching all files if no pattern is provided
+  local search_pattern = pattern or ""
+
+  -- Use telescope's 'find_files' to search the current directory and subdirectories.
+  -- Telescope uses git ls-files if available, making it very fast.
+  telescope.find_files({
+    -- Start the search with the user-provided pattern already typed in the prompt
+    default_text = search_pattern,
+    -- Search recursively from the current directory
+    cwd = vim.fn.getcwd(),
+    -- Use the default file finder settings (respects .gitignore)
+    find_command = {
+        'rg', '--files', '--hidden',
+        '--glob', '!{.git}',  -- Exclude files in the .git directory explicitly
+    }
+  })
+end
+
+-- --------------------------------------------------------
+-- Create a Vim command to easily call the Lua function from the command line
+-- Usage: :Openf <search_pattern>
+-- --------------------------------------------------------
+vim.api.nvim_create_user_command(
+  'Openf',
+  function(opts)
+    -- opts.args contains the string passed to the command
+    openf(opts.args)
+  end,
+  { nargs = '?' } -- Allows 0 or 1 argument
+)
